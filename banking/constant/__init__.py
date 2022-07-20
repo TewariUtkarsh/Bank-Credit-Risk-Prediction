0bc2ad45faf9eadb8fdf5f27316d179c04d8ab6c
UT <tewariutkarsh@outlook.com>
2022-07-20 16:54:02 +0530

    Updated constants for data ingestion

--- banking/constant/__init__.py
+++ banking/constant/__init__.py
@@ -33,3 +33,15 @@ CONFIG_FILE_PATH = os.path.join(
 TRAINING_PIPELINE_CONFIG_KEY= "training_pipeline_config"
 TRAINING_PIPELINE_NAME_KEY = "pipeline_name"
 TRAINING_PIPELINE_ROOT_ARTIFACT_DIR_KEY = "root_artifact_dir"
+
+
+# Data Ingestion:
+DATA_INGESTION_CONFIG_KEY = "data_ingestion_config"
+DATA_INGESTION_DATASET_DOWNLOAD_URL_KEY = "dataset_download_url"
+DATA_INGESTION_ZIP_DOWNLOAD_DIR_KEY = "zip_download_dir"
+DATA_INGESTION_EXTRACTED_DATA_DIR = "extracted_data_dir"
+DATA_INGESTION_RAW_DATA_DIR_KEY = "raw_data_dir"
+DATA_INGESTION_INGESTED_DATA_DIR_KEY = "ingested_data_dir"
+DATA_INGESTION_INGESTED_TRAIN_DIR_KEY = "ingested_train_dir"
+DATA_INGESTION_INGESTED_TEST_DIR_KEY = "ingested_test_dir"
+DATA_INGESTION_ARTIFACT_DIR = "data_ingestion"
