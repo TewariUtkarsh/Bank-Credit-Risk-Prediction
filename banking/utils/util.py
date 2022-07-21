b3c0534068533d35228b1002820ea5d32bca25c3
UT <tewariutkarsh@outlook.com>
2022-07-21 09:41:08 +0530

    Updated utils for Data Validation

--- banking/utils/util.py
+++ banking/utils/util.py
@@ -2,6 +2,7 @@ from banking.exception import BankingException
 from banking.logger import logging
 import yaml
 import os, sys
+import pandas as pd
 
 
 def read_yaml_data(file_path:str) -> dict:
@@ -27,4 +28,22 @@ def read_yaml_data(file_path:str) -> dict:
         raise BankingException(e, sys) from e
 
     
+def save_df_to_csv(data: pd.DataFrame(), file_path:str) -> None:
+    """
+    This function is responsible for saving the DataFrame passed to
+    the desired location.
+    Parameters:
+    -----------
+    data : pandas.DataFrame()
+        DataFrame object of the dataset to be saved.
+    file_path : str
+        File path for saving Data.
+    """   
+    try:
+        file_dir = os.path.dirname(file_path)
+        os.makedirs(file_dir, exist_ok=True)
+        data.to_csv(file_path, index=None)
+        logging.info(f"Saving DataFrame at: [{file_path}]")
+    except Exception as e:
+        raise BankingException(e,sys) from e
 
