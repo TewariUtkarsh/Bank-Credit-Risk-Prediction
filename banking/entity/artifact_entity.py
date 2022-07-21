c2b300bb82d79519030b80422ac0b831bc3478ea
UT <tewariutkarsh@outlook.com>
2022-07-21 09:33:24 +0530

    Updated artifact entity for data validation

--- banking/entity/artifact_entity.py
+++ banking/entity/artifact_entity.py
@@ -7,3 +7,13 @@ DataIngestionArtifact = namedtuple(
     "DataIngestionArtifact",
     ["is_ingested", "message", "train_data_file_path", "test_data_file_path"]
 )
+
+
+# Data Validation Artifact
+DataValidationArtifact = namedtuple(
+    "DataValidationArtifact",
+    ["is_validated", "message", "schema_file_path", "report_file_path", "report_page_file_path"]
+)
+
+
+
