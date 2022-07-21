a9e8f24d849f00c9c5e2abb96fdc534c063b175d
UT <tewariutkarsh@outlook.com>
2022-07-21 09:42:20 +0530

    Updated Pipeline for Data Validation

--- banking/pipeline/pipeline.py
+++ banking/pipeline/pipeline.py
@@ -1,10 +1,11 @@
 import os, sys
 from banking.logger import logging
 from banking.exception import BankingException
-from banking.entity.config_entity import DataIngestionConfig
-from banking.entity.artifact_entity import DataIngestionArtifact
+from banking.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from banking.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
 from banking.config.configuration import Configuration
 from banking.component.data_ingestion import DataIngestion
+from banking.component.data_validation import DataValidation
 
 class Pipeline:
 
@@ -13,14 +14,20 @@ class Pipeline:
         
 
     def start_data_ingestion(self) -> DataIngestionArtifact:
-        
         data_ingestion = DataIngestion(data_ingestion_config=self.config.get_data_ingestion_config())
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
         return data_ingestion_artifact
 
+    def start_data_validation(self, DataIngestionArtifact: DataIngestionArtifact) -> DataValidationArtifact:
+        data_validation = DataValidation(DataValidationConfig=self.config.get_data_validation_config(), DataIngestionArtifact=DataIngestionArtifact)
+        data_validation_artifact = data_validation.initiate_data_validation()
+        return data_validation_artifact
+
 
     def run_pipeline(self):
+        logging.info(f"Initiating the Pipeline.")
         data_ingestion_artifact = self.start_data_ingestion()
+        data_validation_artifact = self.start_data_validation(DataIngestionArtifact=data_ingestion_artifact)
 
     def __del__(self):
         pass
