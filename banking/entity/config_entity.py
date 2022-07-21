c3358cd07dd2964971af51e7d2f816cd885dc90e
UT <tewariutkarsh@outlook.com>
2022-07-21 09:27:40 +0530

    Updated config_entity.py

--- banking/entity/config_entity.py
+++ banking/entity/config_entity.py
@@ -15,5 +15,10 @@ DataIngestionConfig = namedtuple(
 )
 
 
+# Data Validation Config
+DataValidationConfig = namedtuple(
+    "DataValidationConfig",
+    ["schema_file_path", "report_file_path", "report_page_file_path"]
+)
 
 
