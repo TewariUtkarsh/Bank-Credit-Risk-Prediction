7d1718d326e5ea6244fc0c862c4dab1d543e0727
UT <tewariutkarsh@outlook.com>
2022-07-26 16:57:39 +0530

    Updated logger

--- banking/logger/__init__.py
+++ banking/logger/__init__.py
@@ -1,6 +1,9 @@
 import logging
-import os
+import os, sys
 from banking.constant import *
+import pandas as pd
+
+from banking.exception import BankingException
 
 
 os.makedirs(LOGGING_DIR, exist_ok=True)
@@ -32,3 +35,28 @@ logging.basicConfig(
 
 
 # Get Logs Dataframe
+def get_logs_dataframe() -> pd.DataFrame:
+    """
+    This function is responsible for returning the dataframe for the
+    log file.
+
+    Returns
+    -------
+    log_df : pd.DataFrame
+        Dataframe for the log file.
+    """
+    try:
+        log_file_path = LOGGING_FILEPATH
+
+        rows = []
+        with open(log_file_path, 'r') as file_obj:
+            for line in file_obj.readlines():
+                rows.append(line.split('^;'))
+
+        columns=["Time stamp","Log Level","line number","file name","function name","message"]
+        log_df = pd.DataFrame(rows, columns=columns)
+        log_df["log_message"] = log_df['Time stamp'].astype(str) +" :$ "+ log_df["message"]
+        return log_df
+    except Exception as e:
+        raise BankingException(e, sys)
+
